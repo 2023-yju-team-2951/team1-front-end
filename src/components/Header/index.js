033be0cdeb7308772d5893907774b4206e69a1e4
hetame1 <poi8946@naver.com>
2023-05-26 23:19:38 +0900

    1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import './Header.css';
-import '../Story'
+import '../Story';
 
 class Header extends HTMLElement {
   constructor() { 
@@ -7,10 +7,13 @@ class Header extends HTMLElement {
     
     this.innerHTML = 
     `
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <div class="main">
       <div class="main-body">
-        <div class="story-wrapper">
-          <story-component></story-component>
+        <div class="menu-wrapper">
+          <div class="story-wrapper">
+            <story-component></story-component>
+          </div>
         </div>
       </div>
     </div>
