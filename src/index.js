7ff31450283df070c462d4a171ef0991f7a66238
hetame1 <poi8946@naver.com>
2023-05-30 11:49:20 +0900

    1

--- src/index.js
+++ src/index.js
@@ -51,7 +51,9 @@ const router = async () => {
 }
 
 // 뒤로가기나 새로고침을 했을 때 router함수 실행
-window.addEventListener('popstate', router)
+window.addEventListener('popstate', () => {
+  router(); 
+})
 
 // 렌더링 되면 router함수 실행
 document.addEventListener('DOMContentLoaded', () => {
