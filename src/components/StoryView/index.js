eb6e0bf81e565bbecacbd565bcfe9074a94b516e
hetame1 <poi8946@naver.com>
2023-05-30 23:41:05 +0900

    1

--- src/components/StoryView/index.js
+++ src/components/StoryView/index.js
@@ -19,51 +19,57 @@ class StoryView extends HTMLElement {
   }
 
   render() {
-
+    const storyContainer = document.createElement("div");
+    storyContainer.className = "story-container";
+  
+    const centerStory = document.createElement("div");
+    centerStory.className = "center-story";
+  
+    const leftStories = document.createElement("div");
+    leftStories.className = "left-stories";
+  
+    const rightStories = document.createElement("div");
+    rightStories.className = "right-stories";
+  
+    this.appendChild(storyContainer);
+    storyContainer.appendChild(centerStory);
+    storyContainer.appendChild(leftStories);
+    storyContainer.appendChild(rightStories);
+  
     let translateXValue = 0;
-
+  
     this.data.forEach((story) => {
-
-      const storyContainer = document.createElement("div");
-      storyContainer.className = "story-modal-container";
-
-      const translateX = `translateX(${translateXValue}px)`
-      storyContainer.style.transform = translateX;
-
-      translateXValue += 300;
-
       const storyElement = document.createElement("section");
-      storyElement.className = "story";
-
+      storyElement.className = "modal-story";
+  
       const storyItem = document.createElement("div");
       storyItem.className = "story-item";
-
+  
       const btnElement = document.createElement("button");
       btnElement.className = "slide-button prevB";
-
+  
       const btnElement2 = document.createElement("button");
       btnElement2.className = "slide-button nextB";
-
+  
       const btnDiv = document.createElement("div");
       btnDiv.className = "btn-prev";
-
+  
       const btnDiv2 = document.createElement("div");
       btnDiv2.className = "btn-next";
-
+  
       const carouselElement = document.createElement("div");
       carouselElement.className = "carousel slide";
       carouselElement.id = "imgCarousel";
-
+  
       const carouselInner = document.createElement("div");
       carouselInner.className = "carousel-inner";
-
+  
       const carouselItem = document.createElement("div");
       carouselItem.className = "carousel-item active";
-
+  
       const imgElement = document.createElement("img");
       imgElement.src = story.img;
-
-      this.appendChild(storyContainer);
+  
       storyContainer.appendChild(storyElement);
       storyElement.appendChild(storyItem);
       storyItem.appendChild(btnElement);
@@ -74,10 +80,20 @@ class StoryView extends HTMLElement {
       carouselElement.appendChild(carouselInner);
       carouselInner.appendChild(carouselItem);
       carouselItem.appendChild(imgElement);
-
-
-    })
+  
+      // 스토리 요소를 중앙, 왼쪽, 오른쪽에 추가
+      if (translateXValue === 0) {
+        centerStory.appendChild(storyElement);
+      } else if (translateXValue < 0) {
+        leftStories.appendChild(storyElement);
+      } else {
+        rightStories.appendChild(storyElement);
+      }
+  
+      translateXValue += 300;
+    });
   }
+  
 }
 
 window.customElements.define('storyview-component', StoryView);
