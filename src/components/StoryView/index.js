3da876cd556dd8f92c6115ffa153026d51b1e5c6
hetame1 <poi8946@naver.com>
2023-06-01 12:56:53 +0900

    1

--- src/components/StoryView/index.js
+++ src/components/StoryView/index.js
@@ -1,25 +1,70 @@
-import './storyview.css'
+import './storyView.css'
+import { data } from '../Story/dataTest.js'
 
 class StoryView extends HTMLElement {
   constructor() { 
     super();
-
-    this.data = [
-      {
-        img: "./assets/image/imyon.jpg",
-        name: "imyon"
-      },
-      {
-        img: "./assets/image/vaundy.jpg",
-        name: "vaundy"
-      },
-    ]
-
-    this.render()
+    this.data = data;
+    this.storyWrapper = document.createElement('div');
+    this.storyWrapper.className = 'story-modal-wrapper';
+    this.modalWrapper = document.createElement('div');
+    this.modalWrapper.className = 'modal-wrapper';
+    this.render();
   }
 
   render() {
-    
+    this.appendChild(this.storyWrapper);
+    this.storyWrapper.appendChild(this.modalWrapper);
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = parseInt(urlParams.get('id'));
+    console.log(id);
+    // Find the index of the data with the corresponding ID
+    const index = this.data.findIndex(item => item.id === id);
+
+    // If index is not -1, the ID was found in the data array
+    if(index !== -1) {
+
+      // If there is a previous element, create a side story for it
+      if(index > 0) {
+        const prevData = this.data[index - 1];
+        this.createStoryContainer(prevData, 'side-story');
+      }
+
+      const middleData = this.data[index];
+      
+      this.createStoryContainer(middleData, 'story');
+
+      // If there is a next element, create a side story for it
+      if(index < this.data.length - 1) {
+        const nextData = this.data[index + 1];
+        this.createStoryContainer(nextData, 'side-story');
+      }
+    }
+
+  }
+
+  createStoryContainer(data, className) {
+    const container = document.createElement('div');
+    container.className = `story-container`;
+
+    if(className === 'story') {
+      container.style.flexGrow = 1;
+    }
+
+    this.modalWrapper.appendChild(container);
+
+    if (className !== 'side-story') {
+      const modalStory = document.createElement('div');
+      modalStory.className = 'modal-story';
+
+      container.appendChild(modalStory);
+    } else {
+      const sideStory = document.createElement('div');
+      sideStory.className = 'side-story';
+
+      container.appendChild(sideStory);
+    }
   }
   
 }
