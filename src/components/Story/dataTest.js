ddd1c838bb82361a40ebb0cb70138ab327072345
hetame1 <poi8946@naver.com>
2023-06-05 19:07:48 +0900

    1

--- src/components/Story/dataTest.js
+++ src/components/Story/dataTest.js
@@ -1,111 +1,88 @@
 export const data = [
   {
     id: 1,
-    img: "./assets/image/imyon.jpg",
+    img: "./assets/image/TestImage/imyon.jpg",
     name: "imyon",
     storyImg: [
-      "./assets/image/imyon.jpg",
-      "./assets/image/vaundy.jpg",
+      "./assets/image/TestImage/forest.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
+    ],
+    storyText: [
+      "qweqwe",
+      "안녕하세요 f asfasf"
     ]
   },
   {
     id: 2,
-    img: "./assets/image/vaundy.jpg",
+    img: "./assets/image/TestImage/vaundy.jpg",
     name: "vaundy",
     storyImg: [
-      "./assets/image/vaundy.jpg",
-      "./assets/image/imyon.jpg",
+      "./assets/image/TestImage/bubbles.jpg",
+      "./assets/image/TestImage/imyon.jpg",
     ]
     
   },
   {
     id: 3,
-    img: "./assets/image/imyon.jpg",
+    img: "./assets/image/TestImage/imyon.jpg",
     name: "imyon",
     storyImg: [
-      "./assets/image/imyon.jpg",
-      "./assets/image/vaundy.jpg",
+      "./assets/image/TestImage/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
     ]
   },
   {
     id: 4,
-    img: "./assets/image/vaundy.jpg",
+    img: "./assets/image/TestImage/vaundy.jpg",
     name: "vaundy",
     storyImg: [
-      "./assets/image/vaundy.jpg",
-      "./assets/image/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
+      "./assets/image/TestImage/imyon.jpg",
     ]
   },
   {
     id: 5,
-    img: "./assets/image/imyon.jpg",
+    img: "./assets/image/TestImage/imyon.jpg",
     name: "imyon",
     storyImg: [
-      "./assets/image/imyon.jpg",
-      "./assets/image/vaundy.jpg",
+      "./assets/image/TestImage/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
     ]
   },
   {
     id: 6,
-    img: "./assets/image/vaundy.jpg",
+    img: "./assets/image/TestImage/vaundy.jpg",
     name: "vaundy",
     storyImg: [
-      "./assets/image/vaundy.jpg",
-      "./assets/image/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
+      "./assets/image/TestImage/imyon.jpg",
     ]
   },
   {
     id: 7,
-    img: "./assets/image/imyon.jpg",
+    img: "./assets/image/TestImage/imyon.jpg",
     name: "imyon",
     storyImg: [
-      "./assets/image/imyon.jpg",
-      "./assets/image/vaundy.jpg",
+      "./assets/image/TestImage/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
     ]
   },
   {
     id: 8,
-    img: "./assets/image/vaundy.jpg",
+    img: "./assets/image/TestImage/vaundy.jpg",
     name: "vaundy",
     storyImg: [
-      "./assets/image/vaundy.jpg",
-      "./assets/image/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
+      "./assets/image/TestImage/imyon.jpg",
     ]
   },
   {
     id: 9,
-    img: "./assets/image/imyon.jpg",
+    img: "./assets/image/TestImage/imyon.jpg",
     name: "imyon",
     storyImg: [
-      "./assets/image/imyon.jpg",
-      "./assets/image/vaundy.jpg",
-    ]
-  },
-  {
-    id: 10,
-    img: "./assets/image/vaundy.jpg",
-    name: "vaundy",
-    storyImg: [
-      "./assets/image/vaundy.jpg",
-      "./assets/image/imyon.jpg",
-    ]
-  },
-  {
-    id: 11,
-    img: "./assets/image/imyon.jpg",
-    name: "imyon",
-    storyImg: [
-      "./assets/image/imyon.jpg",
-      "./assets/image/vaundy.jpg",
-    ]
-  },
-  {
-    id: 12,
-    img: "./assets/image/vaundy.jpg",
-    name: "vaundy",
-    storyImg: [
-      "./assets/image/vaundy.jpg",
-      "./assets/image/imyon.jpg",
+      "./assets/image/TestImage/imyon.jpg",
+      "./assets/image/TestImage/vaundy.jpg",
     ]
   },
 ]
