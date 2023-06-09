7c1afb1bacf8ad765866d7d640e92824667c35fa
hetame1 <poi8946@naver.com>
2023-06-09 18:08:11 +0900

    1

--- src/components/StoryModal/index.js
+++ src/components/StoryModal/index.js
@@ -9,8 +9,6 @@ class StoryModal extends HTMLDivElement {
     this.setAttribute('aria-hidden', 'true');
     this.setAttribute('tabindex', '-1');
 
-    this.pick = '';
-
     this.num = 0;
 
     this.innerHTML = `
@@ -23,7 +21,7 @@ class StoryModal extends HTMLDivElement {
         <div class="modal-body">
           <div id="num-0" class="num">
             <div class="color-picker">
-              <div class="colors" style="background-color: #ffffff;">White</div>
+              <div class="colors selected" style="background-color: #ffffff;"></div>
               <div class="colors" style="background-color: #ffadad;"></div>
               <div class="colors" style="background-color: #ffd6a5;"></div>
               <div class="colors" style="background-color: #fdffb6;"></div>
@@ -35,9 +33,17 @@ class StoryModal extends HTMLDivElement {
             </div>
           </div>
           <div id="num-1" class="num" style="display: none;">
+
             <div class="writing">
-              <input type="text" class="text-write" />
+              <textarea class="text-write"></textarea>
             </div>
+
+            <div class="font-color">
+              <div class="fontColors" style="background-color: #ffffff;"></div>
+              <div class="fontColors selected" style="background-color: #000000;"></div>
+              <div class="fontColors" style="background-color: #2f4f4f;"></div>
+            </div>
+
           </div>
         </div>
         <div class="modal-footer">
@@ -47,7 +53,7 @@ class StoryModal extends HTMLDivElement {
           <button id="finish-button" type="button" class="btn btn-primary" style="display: none;">Finish</button>
         </div>
       </div>
-    </div>`;
+    </div>`
 
     // 다음 버튼 클릭시
     this.querySelector('#next-button').addEventListener('click', () => {
@@ -73,33 +79,64 @@ class StoryModal extends HTMLDivElement {
       this.updateNum();
     });
 
-    // 색깔 고를때 효과 + 고른 색 selected 클래스 추가
+    // 완료 버튼 클릭시
+    this.querySelector('#finish-button').addEventListener('click', () => {
+      const textWrite = this.querySelector('.text-write');
+      const writingElement = this.querySelector('.writing')
+
+      // json-server post 요청
+      fetch('http://localhost:7000/profiles/1')
+        .then((res) => res.json())
+        .then((user) => {
+          user.storyImg.push(writingElement.style.background);
+          console.log(user.storyImg);
+        })
+    });
+
+    // 색깔 고를때 효과 + 고른 색 selected 클래스 추가 + 고른색으로 배경 변경
     this.colors = this.querySelectorAll('.colors')
     this.colors.forEach((color) => {
       color.addEventListener('click', (e) => {
         this.pick = e.target.style.backgroundColor;
 
-        const colorSelected = e.target.classList.contains('selected');
-
         this.colors.forEach((color) => {
           color.classList.remove('selected');
         });
 
-        if (!colorSelected) {
-          e.target.classList.add('selected');
-        }
-
+        e.target.classList.add('selected');
       });
     });
 
+    // 글자색 변경
+    this.fontColors = this.querySelectorAll('.fontColors')
+    this.fontColors.forEach((fColor) => {
+      fColor.addEventListener('click', (e) => {
+        this.pick = e.target.style.backgroundColor;
+
+        const textWrite = this.querySelector('.text-write');
+        textWrite.style.color = this.pick;
+
+        this.fontColors.forEach((fColor) => {
+          fColor.classList.remove('selected');
+        });
+
+        e.target.classList.add('selected');
+      });
+    });
+
+    // 배경 클릭해도 입력창 클릭되게
     const writingElement = this.querySelector('.writing')
     writingElement.addEventListener('click', () => {
       this.querySelector('.text-write').focus();
     });
 
+    // 인풋창 높이 계속 변경해주기기
     const textInput = this.querySelector('.text-write');
-
-    
+    textInput.addEventListener('input', function () {
+      this.style.display = 'block';
+      this.style.height = (this.scrollHeight) + 'px';
+    });
+        
   }
 
   // 다음, 이전 버튼 클릭시 화면 업데이트
