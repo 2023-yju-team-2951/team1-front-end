6fa7085b57bdde0d9987033298dfde6e540b9d5c
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-06-13 04:14:17 +0900

    refactor(SearchModal): restructure based on modal replacement method

--- src/components/Modal/SearchModal/index.js
+++ src/components/Modal/SearchModal/index.js
@@ -1,26 +1,25 @@
 import './SearchModal.css';
-import Modal from '../../Modal';
-class SearchModal extends Modal {
+class SearchModal extends HTMLDivElement {
   constructor() {
     super();
-    this.classList.add('search-modal');
+    this.classList.add('search-modal', 'modal-dialog');
+    this.setAttribute('role', 'document');
 
+    // TODO: 미완성
     this.innerHTML = `
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">검색</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>Modal body text goes here.</p>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Save changes</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          </div>
-          </div>
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">검색</h5>
+          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
         </div>
       </div>
     `;
