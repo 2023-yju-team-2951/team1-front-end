3c5f3ede3899ee9a424ceb83c12e2338adf1a882
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-06-12 19:59:16 +0900

    minus(package.json): removed webpack-dev-server dependency

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,6 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: './src/index.js',
 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 3000,
-  },
-
   output: {
     path: path.resolve('public/assets/js/dist'),
     filename: 'bundle.js',
