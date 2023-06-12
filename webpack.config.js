f8cbae069c5737589155a03482d28e0aeba26696
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-06-12 20:35:50 +0900

    plus(package.json): add webpack-dev-server dependency

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: './src/index.js',
 
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+  watchOptions: {
+    poll: true,
+    ignored: '/node_modules/',
+  },
+
   output: {
     path: path.resolve('public/assets/js/dist'),
     filename: 'bundle.js',
