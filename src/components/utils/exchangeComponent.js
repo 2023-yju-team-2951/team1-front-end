2cd125f85f2be79a381cbc3aff6b7e267beabe4e
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-06-18 07:44:59 +0900

    docs(request): add JSDoc for function

--- src/components/utils/exchangeComponent.js
+++ src/components/utils/exchangeComponent.js
@@ -1,3 +1,8 @@
+/**
+ * oldComponent와 newComponent를 교체합니다.
+ * @param {Node} oldComponent
+ * @param {Node} newComponent
+ */
 export function exchangeComponent(oldComponent, newComponent) {
   oldComponent.parentNode.replaceChild(newComponent, oldComponent);
 }
